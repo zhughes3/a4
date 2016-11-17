40871f0e7d9286086240562664ad8df86b5b961e
zhughes3 <zhughes3@gmail.com>
2016-11-17 01:18:51 -0500

    Added file upload AJAX call

--- api/ImageEndpoint.php
+++ api/ImageEndpoint.php
@@ -1,31 +1,37 @@
 <?php
-
-if (isset($_REQUEST[]))
-
-else if (isset($_REQUEST['json'])) {
-    $decoded = json_decode(stripslashes($_REQUEST['json']), TRUE);
-    if (is_null($decoded)) {
-        $response['status'] = array(
-            'type' => 'error',
-            'value' => 'Invalid JSON value found',
-        );
-        $response['request'] = $_REQUEST['json'];
+if(isset($_FILES["file"]["type"]))
+{
+    $validextensions = array("jpeg", "jpg", "png");
+    $temporary = explode(".", $_FILES["file"]["name"]);
+    $file_extension = end($temporary);
+    if ((($_FILES["file"]["type"] == "image/png") || ($_FILES["file"]["type"] == "image/jpg") || ($_FILES["file"]["type"] == "image/jpeg")
+        ) && ($_FILES["file"]["size"] < 100000)//Approx. 100kb files can be uploaded.
+        && in_array($file_extension, $validextensions)) {
+        if ($_FILES["file"]["error"] > 0)
+        {
+            echo "Return Code: " . $_FILES["file"]["error"] . "<br/><br/>";
+        }
+        else
+        {
+            if (file_exists("upload/" . $_FILES["file"]["name"])) {
+                echo $_FILES["file"]["name"] . " <span id='invalid'><b>already exists.</b></span> ";
+            }
+            else
+            {
+                $sourcePath = $_FILES['file']['tmp_name']; // Storing source path of the file in a variable
+                $targetPath = "upload/".$_FILES['file']['name']; // Target path where file is to be stored
+                move_uploaded_file($sourcePath,$targetPath) ; // Moving Uploaded file
+                echo "<span id='success'>Image Uploaded Successfully...!!</span><br/>";
+                echo "<br/><b>File Name:</b> " . $_FILES["file"]["name"] . "<br>";
+                echo "<b>Type:</b> " . $_FILES["file"]["type"] . "<br>";
+                echo "<b>Size:</b> " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+                echo "<b>Temp file:</b> " . $_FILES["file"]["tmp_name"] . "<br>";
+            }
+        }
     }
-    else {
-        $response['status'] = array(
-            'type' => 'message',
-            'value' => 'Valid JSON value found',
-        );
-        //Send the original message back.
-        $response['request'] = $decoded;
+    else
+    {
+        echo "<span id='invalid'>***Invalid file Size or Type***<span>";
     }
 }
-else {
-    $response['status'] = array(
-        'type' => 'error',
-        'value' => 'No JSON value set',
-    );
-}
-$encoded = json_encode($response);
-header('Content-type: application/json');
-exit($encoded);
+?>
