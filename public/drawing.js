c281957b6a5a192781cc8c456da84787f6c6cd07
Erin Boehlert <boehlert@email.unc.edu>
2016-12-05 13:41:27 -0500

    fixed drawing functionality and minor style changes

--- public/drawing.js
+++ public/drawing.js
@@ -1,65 +1,10 @@
-$(document).ready(function() {
 
-	 $('#canvas').mousedown(function(e){
-        var mouseX = e.pageX - this.offsetLeft;
-        var mouseY = e.pageY - this.offsetTop;
-
-        paint = true;
-        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-        redraw();
-    });
-
-    $('#canvas').mousemove(function(e){
-        if(paint){
-            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-            redraw();
-        }
-    });
-
-    $('#canvas').mouseup(function(e){
-        paint = false;
-    });
-
-    $('#canvas').mouseleave(function(e){
-        paint = false;
-    });
-   
-   $('#clearCanvas').on('click', function(){
-        context.clearRect(0, 0, context.canvas.width, context.canvas.height); 
-        clickX = [];
-        clickY = [];
-        clickDrag = [];
-        clickColor = [];
-        clickSize = [];
-   });
-
-   $('#smallPen').on('click', function(){
-        curSize = 5;
-   });
-
-   $('#mediumPen').on('click', function(){
-        curSize = 10;
-   });
-
-   $('#largePen').on('click', function(){
-        curSize = 15;
-   });
-
-   $('#XLPen').on('click', function(){
-        curSize = 20;
-   });
-
-});
-
-
-
-var canvasContainer = $('#canvas-container');
-var canvas = $('#canvas');
+// var canvasContainer = $('#canvas-container');
+var canvas = document.getElementById('canvas');
+canvas.setAttribute('width', '800px');
+canvas.setAttribute('height', '500px');
+var context = canvas.getContext('2d');
 var colorPicker = $('#color-picker');
-canvas.attr('width', '800px');
-canvas.attr('height', '500px');
-context = canvas[0].getContext('2d');
-
 var clickX = new Array();
 var clickY = new Array();
 var clickDrag = new Array();
@@ -69,8 +14,8 @@ var clickSize = new Array();
 var curColor, curSize;
 
 function addClick(x, y, dragging){
-    clickX.push(x - 380);
-    clickY.push(y - 110);
+    clickX.push(x);
+    clickY.push(y);
     clickDrag.push(dragging);
     curColor = colorPicker.val()
     clickColor.push(curColor);
@@ -94,4 +39,69 @@ function redraw(){
         context.stroke();
     }
 }
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+	    x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+
+canvas.addEventListener('mousemove', function(evt) {
+	var mousePos = getMousePos(canvas, evt);
+	if(paint){
+		addClick(mousePos.x, mousePos.y, true);
+		redraw();
+	}
+}, false);
+
+canvas.addEventListener('mousedown', function(evt) {
+	var mousePos = getMousePos(canvas, evt);
+	paint = true;
+	addClick(mousePos.x, mousePos.y);
+	redraw();
+});
+
+canvas.addEventListener('mouseup', function(evt) {
+	paint = false;
+});
+
+canvas.addEventListener('mouseleave', function(evt) {
+	paint = false;
+});
+
+$('#clearCanvas').on('click', function(){
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height); 
+        clickX = [];
+        clickY = [];
+        clickDrag = [];
+        clickColor = [];
+        clickSize = [];
+   });
+
+$('#smallPen').on('click', function(){
+    curSize = 5;
+});
+
+$('#mediumPen').on('click', function(){
+    curSize = 10;
+});
+
+$('#largePen').on('click', function(){
+    curSize = 15;
+});
+
+$('#XLPen').on('click', function(){
+    curSize = 20;
+});
+
+$('#canvasSettings').on('click', '.btn-default', function(){
+   	$('.btn').removeClass('btn-primary');
+   	$('.btn').addClass('btn-default');
+   	$(this).removeClass('btn-default');
+    $(this).addClass('btn-primary');
+});
+
+
 
